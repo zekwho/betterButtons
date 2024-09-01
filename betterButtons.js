6b4cea6f90bff143d931073bea8b6350b5cdd60d
zekwho <112750206+zekwho@users.noreply.github.com>
2024-09-01 00:23:47 -0400

    Update betterButtons.js

--- betterButtons.js
+++ betterButtons.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         betterButtons
 // @namespace    https://github.com/zekwho/betterButtons
-// @version      1.2
+// @version      1.0
 // @description  Adds dynamic elements to buttons
 // @author       ZEK [3369]
 // @match        https://cartelempire.online/*
@@ -75,7 +75,7 @@ window.onload = (function() {
         }
     `);
     function applyGradientAnimationToButtons() {
-        const buttons = document.querySelectorAll('.btn-success, .btn-danger');   //i have small ween0r
+        const buttons = document.querySelectorAll('.btn-success, .btn-danger');   
         buttons.forEach(button => {
             if (button.classList.contains('btn-success')) {
                 button.classList.add('job-button-gradient');
